2e95f4f9f5c947be80684d10e0e1f714e123fde4
Sebastian Kindler <kindler@mogwailabs.de>
2019-04-25 10:12:16 +0200

    Refactoring: Mogwai Labs

--- payloads_src/siberas_mlet/src/de/siberas/lab/SiberasPayloadMBean.java
+++
@@ -1,9 +0,0 @@
- package de.siberas.lab;
-
-public interface SiberasPayloadMBean {
-	
-	public String runCMD(String passwd, String cmd);
-	public String runJS(String passwd, String js);
-	public boolean changePassword(String oldPass, String newPass);
-	
-}
